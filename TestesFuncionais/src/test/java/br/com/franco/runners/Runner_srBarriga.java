748e6a0054355c1de524c14c9e1002aab35b887c
FrancoRoldao <francotrabalhoestudo@gmail.com>
2023-01-31 10:02:19 -0300

    atualizando estrutura de pastas e pacotes.

--- TestesFuncionais/src/test/java/br/com/franco/runners/Runner_srBarriga.java
+++ TestesFuncionais/src/test/java/br/com/franco/runners/Runner_srBarriga.java
@@ -1,4 +1,4 @@
-package runners;
+package br.com.franco.runners;
 
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
@@ -8,9 +8,9 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/features/srBarriga/login.feature",
-		glue = "steps",
-        plugin = {"pretty", },
+		features = "src/test/resources/features/srBarriga/login.feature",
+		glue = "br.com.franco.steps",
+        plugin = {"pretty", "html:target/report-html", "json:target/report.json" },
         publish = false,
 		stepNotifications = true,
 		dryRun = false,
--- TestesFuncionais/src/test/java/br/com/franco/steps/Steps_srBarriga.java
+++ TestesFuncionais/src/test/java/br/com/franco/steps/Steps_srBarriga.java
@@ -1,4 +1,4 @@
-package steps;
+package br.com.franco.steps;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
