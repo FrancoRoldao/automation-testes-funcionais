487e56026dd118bfaa9e0a889fdf059b17fd7a6a
FrancoRoldao <francotrabalhoestudo@gmail.com>
2023-02-08 15:21:02 -0300

    ajustando o nome da feature+steps de "movimenta" para "movimentacao"

--- TestesFuncionais/src/test/java/br/com/franco/runners/Runner_srBarriga.java
+++ TestesFuncionais/src/test/java/br/com/franco/runners/Runner_srBarriga.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/features/srBarriga/movimenta.feature",
+		features = "src/test/resources/features/login.feature",
 		glue = "br.com.franco.steps",
         plugin = {"pretty", "html:target/report-html", "json:target/report.json" },
         publish = false,
